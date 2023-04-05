14f950b317b84b632232b224b216643d623242fe
jean2030 <jcg86@live.com>
2023-04-04 23:57:13 -0300

    Utilizando primeiro component de POUI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+import { PoMenuItem } from '@po-ui/ng-components';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'crud-angular';
+
+  table_list = [{ table: 'PO Table', angular: 'PO-UI' },
+                { table: 'PO Table', angular: 'PO-UI' },  
+                { table: 'PO Table', angular: 'PO-UI' },
+                { table: 'PO Table', angular: 'PO-UI' }, 
+                { table: 'PO Table', angular: 'PO-UI' }, 
+                { table: 'PO Table', angular: 'PO-UI' }]
+  readonly menus: Array<PoMenuItem> = [
+    { label: 'Home', action: this.onClick.bind(this) }
+  ];
+
+  private onClick() {
+    alert('Clicked in menu item')
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PoModule } from '@po-ui/ng-components';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PoModule,
+    HttpClientModule,
+    RouterModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
